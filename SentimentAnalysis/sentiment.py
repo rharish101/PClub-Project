634711d851eb5a7102c44cad74d36b53aa68e279
rharish101 <harish.rajagopals@gmail.com>
2017-07-01 08:54:28 +0530

    Restrict training data for bigrams

--- SentimentAnalysis/sentiment.py
+++ SentimentAnalysis/sentiment.py
@@ -1,10 +1,13 @@
 #!/bin/python2
 import codecs
 import numpy as np
-from gensim.parsing.preprocessing import preprocess_string
+from gensim.parsing.preprocessing import preprocess_string, strip_punctuation,\
+                                         stem_text
 
 def export():
-    data_file = codecs.open('Sentiment140/training.1600000.processed.'\
+
+    print "Extracting data..."
+    data_file = codecs.open('Sentiment140/training.1600000.processed.'
                             'noemoticon.csv', encoding='ISO-8859-1')
     data = []
     for tweet in data_file.read().split('\n')[:-1]:
@@ -13,10 +16,25 @@ def export():
     data_file.close()
     labels = np.array([tweet[0] for tweet in data])
     tweets = [tweet[-1] for tweet in data]
+
+    print "Preprocessing data..."
     for i, tweet in enumerate(tweets):
         new_tweet = ' '.join([word for word in tweet.split(' ') if len(word)\
-                              > 0 and word[0] != '@' and 'http' not in word])
-        tweets[i] = np.array(preprocess_string(new_tweet))
+                              > 0 and word[0] not in ['@', '#'] and 'http' not\
+                              in word]).strip()
+        pro_tweet = np.array(preprocess_string(new_tweet))
+        if len(pro_tweet) < 2:
+            tweets[i] = np.array(strip_punctuation(stem_text(new_tweet.lower()
+                                 )).strip().split())
+        else:
+            tweets[i] = pro_tweet
+    backup = np.array(tweets)
+    tweets = []
+    for tweet in backup:
+        if len(tweet) >= 2:
+            tweets.append(tweet)
     tweets = np.array(tweets)
+    del backup
+
     return (tweets, labels)
 
